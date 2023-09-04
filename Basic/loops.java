d0d6e57aab2a4fad0a0628e835de872f3b62e8b5
Vansh Garg <vanshgarg7017@gmail.com>
2023-09-04 23:04:33 +0530

    Initial Commit

---
+++ Basic/loops.java
@@ -0,0 +1,34 @@
+package Basic;
+
+public class loops {
+    public static void main(String[] args) {
+        // int rows, colums;
+        // for (rows = 1; rows <= 4; rows++) {
+        // System.out.println("****");
+
+        // }
+
+        // print the sum of first n natural numbers
+        int n = 5;
+        int sum = 0;
+        int count = 1;
+        // using while loop
+
+        // while (count <= n) {
+        // sum += count;
+        // count++;
+        // }
+        // System.out.println(sum);
+
+        // using forLoop
+
+        // for (count = 1; count <= n; count++) {
+        // sum += count;
+        // }
+        // System.out.println(sum);
+
+        // print reverse of the number
+
+    }
+
+}
---
+++ Basic/operator.java
@@ -0,0 +1,33 @@
+package Basic;
+
+public class operator {
+    public static void main(String[] args) {
+        // int x = 2, y = 5;
+        // int exp1 = (x * y / x);
+        // int exp2 = (x * (y / x));
+        // System.out.println(exp1);
+        // System.out.println(exp2);
+
+        // Comparison Operator
+
+        // int x1 = 200, y1 = 50, z1 = 100;
+        // if (x1 > y1 && y1 > z1) {
+        // System.out.println("Hello");
+        // }
+        // if (z1 > y1 && z1 < x1) {
+        // System.out.println("Java");
+        // }
+        // if ((y1 + 15) < x1 && (y1 + 15) < x1) {
+        // System.out.println("Hello World");
+        // }
+
+        // Assignment Operator
+        // int x2, y2, z2;
+        // x2 = y2 = z2 = 2;
+        // x2 += y2;
+        // y2 -= 1;
+        // z2 += (x2 + y2);
+        // System.out.println(x2 + " " + y2 + " " + z2 + " ");
+
+    }
+}
